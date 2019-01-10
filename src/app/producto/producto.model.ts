8debd86b20d8239446fa6d8fea9a85536858c09b
Javier Garcia <jgp@sens2web.es>
2019-01-10 21:51:05 +0100

    producto model4

---
+++ src/app/producto/producto.model.ts
@@ -0,0 +1,10 @@
+import {Cita} from '../cita/cita.model';
+export interface Producto{
+  id: number;
+  nombre: string;
+  descripcion: string;
+  duracion_min?: number;
+  coste_hora: number;
+
+  citas?: Cita[];
+}
